ac670eea409e04da9068cc9a9f46ecb94c06ea06
mitchthebaker <mitchrpbaker@gmail.com>
2018-11-30 00:52:13 -0800

    Configured database code within index.js and added pg

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res) => {
+  	try {
+  		const client = await pool.connect()
+  		const result = await client.query('SELECT * FROM test_table');
+  		const results = {
+  			'results': (result) ? result.rows : null
+  		};
+  		res.render('pages/db', results);
+  		client.release();
+  	}
+  	catch (err) {
+  		console.error(err);
+  		res.send("Error " + err);
+  	}
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   showTimes = () => {
@@ -20,3 +35,9 @@ express()
   	}
   	return result;
   }
+
+  const { Pool } = require('pg');
+  const pool = new Pool({
+  	connectionString: process.env.DATABASE_URL,
+  	ssl:true
+  });
