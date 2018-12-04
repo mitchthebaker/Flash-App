20755c89218c9e5797c3bdfa73f88d47a03b1afe
mitchthebaker <mitchrpbaker@gmail.com>
2018-12-04 15:35:59 -0800

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 //const app = express(); also works but seems a bit redundant for what I'm doing here
 
 express()
