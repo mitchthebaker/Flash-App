ba6d91015309796acf094253a7e93ae27be80129
mitchthebaker <mitchrpbaker@gmail.com>
2019-01-14 05:24:12 -0800

    Updated index.js, made changes to -success.ejs page to display form data that was posted onto card tag

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const uriString = process.env.MONGODB_URI || 'mongodb://localhost/flashapp';
 const PORT = process.env.PORT || 5001;
+const app = express();
 
 mongoose.connect(uriString, (err, res) => {
   if(err) {
@@ -17,51 +18,49 @@ mongoose.connect(uriString, (err, res) => {
 
 var setSchema = new mongoose.Schema({
   setName: String,
-  term: [String],
-  definition: [String]
-});
+  term: String,
+  definition: String
+}, {strict: false});
 var newSet = mongoose.model('newSet', setSchema);
 
-express()
-  .use(bodyParser.json());
-  .use(bodyParser.urlencoded({
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
     extended: false
-  }));
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(express.static(path.join(__dirname, 'public/scripts')))
-  .set('views', path.join(__dirname, 'views'))
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/scripts')));
+app.set('views', path.join(__dirname, 'views'));
   //.engine('html', require('ejs').renderFile)
-  .set('view engine', 'ejs')
-  .get('/flashHome', (req, res) => res.render('pages/flashcardAppIndex'))
-  .get('/flashCreate', (req, res) => res.render('pages/flashcardAppCreateNewSet'))
-  .get('/flashCreate-success', function(req, res) {
-    res.render('pages/flashcardAppCreateNewSet-success')
-  })
-  .post('/flashCreate', async function(req, res) {
-    console.log(req.body)
-    res.render('pages/flashcardAppCreateNewSet-success', {data: req.body})
-  })
-  .get('/db', async (req, res) => {
-  	try {
-  		const client = await pool.connect()
-  		const result = await client.query('SELECT * FROM test_table');
-  		const results = {
-  			'results': (result) ? result.rows : null
-  		};
+app.set('view engine', 'ejs');
+app.get('/flashHome', (req, res) => res.render('pages/flashcardAppIndex'));
+app.get('/flashCreate', (req, res) => res.render('pages/flashcardAppCreateNewSet'));
+app.get('/flashCreate-success', function(req, res) {
+    res.render('pages/flashcardAppCreateNewSet-success');
+});
+app.post('/flashCreate', async function(req, res) {
+    let set = new newSet(req.body);
+    let setLength = Object.keys(req.body).length;  
+    let setKeys = Object.keys(req.body);
+    let setValues = Object.values(req.body);
+  
+    set.save((err) => {
+      if(!err) {  
+        res.render('pages/flashcardAppCreateNewSet-success', {
+          data: set,
+          keys: setKeys,
+          values: setValues,
+          length: setLength   
+        });
+      }
+      else {
+        res.send('Something went wrong here..');
+        next();
+      }
+      //res.json(set);
+      //res.render('pages/flashcardAppCreateNewSet-success');
+    });
+    
+});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-  		res.render('pages/db', results);
-  		client.release();
-  	}
-  	catch (err) {
-  		console.error(err);
-  		res.send("Error " + err);
-  	}
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  const { Pool } = require('pg');
-  const pool = new Pool({
-  	connectionString: process.env.DATABASE_URL,
-  	ssl:true
-  });
 
